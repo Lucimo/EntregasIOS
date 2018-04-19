fde899d2c24e67ab87e1b8203dca97bdbe07e7f8
Primc <pablocruzmendez@gmail.com>
2018-04-19 13:51:13 +0200

    login yyyy registro

---
+++ practicaIOS/Perfil.swift
@@ -0,0 +1,28 @@
+//
+//  Perfil.swift
+//  practicaIOS
+//
+//  Created by PABLO CRUZ MENDEZ on 19/4/18.
+//  Copyright © 2018 LUCAS PAJARES PRIETO. All rights reserved.
+//
+
+import UIKit
+
+class Perfil: NSObject {
+
+    var sNombre:String?
+    var sEmail:String?
+    
+    func setMap(valores:[String:Any]) {
+        sNombre = valores["nombre"] as? String
+        sEmail = valores["email"] as? String
+    }
+    
+    func getMap() -> [String:Any] {
+        return[
+            "nombre": sNombre as Any,
+            "email": sEmail as Any
+        ]
+    }
+    
+}
---
+++ practicaIOS/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  practicaIOS
+//
+//  Created by LUCAS PAJARES PRIETO on 11/4/18.
+//  Copyright © 2018 LUCAS PAJARES PRIETO. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+    @IBOutlet var btnLoguear:UIButton?
+    @IBOutlet var txtuser:UITextField?
+    @IBOutlet var txtpass:UITextField?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func clicloginevent(){
+        print("Hola " + (txtuser?.text)!)
+        
+        Auth.auth().signIn(withEmail: (txtuser?.text)!, password: (txtpass?.text)!) {(email, error) in
+            if email != nil{
+                
+                let ruta = DataHolder.sharedInstance.fireStoreDB?.collection("Perfiles").document((email?.uid)!)
+                
+                ruta?.getDocument { (document, error) in
+                    if document != nil{
+                    
+                        self.performSegue(withIdentifier: "transitionLogin", sender: self)
+                    }
+                    else{
+                        print(error!)
+                    }
+                }
+            }
+            else{
+                print("Fallo al logearse")
+                print(error!)
+            }
+        }
+        
+        //if txtuser?.text == "Lucas" && txtpass?.text == "pass" {
+      //      self.performSegue(withIdentifier: "transitionLogin", sender: self)       }
+    }
+}
+
