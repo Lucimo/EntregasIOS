ac38e4b46d9829c2c8941b6604ae8624dd47a258
Primc <pablocruzmendez@gmail.com>
2018-05-21 16:57:53 +0200

    seleccionar imagen

---
+++ practicaIOS/VCSelecImg.swift
@@ -0,0 +1,63 @@
+//
+//  VCSelecImg.swift
+//  practicaIOS
+//
+//  Created by PABLO CRUZ MENDEZ on 21/5/18.
+//  Copyright © 2018 LUCAS PAJARES PRIETO. All rights reserved.
+//
+
+import UIKit
+
+class VCSelecImg: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var imgView:UIImageView?
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func accionBotonGaleria(){
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func accionBotonCamara(){
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .camera
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
+        let img = info[UIImagePickerControllerOriginalImage] as? UIImage
+        
+        
+        
+        imgView?.image = img
+        self.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
